cd3dec81af3ccb94c5b391791585244d84c9952d
Sam <paeezan61@gmail.com>
2026-06-21 17:06:44 +0300

    Add daily returns calculation

--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
 from data_loader import download_prices
+from metrics import daily_returns
 
 data = download_prices("AAPL")
 
-print(data.tail())
+returns = daily_returns(data["Close"])
+
+print(returns.tail())
